454f7fe7ba97c0ae5756adeeb01db33e85b42bbf
kencal20 <kenadjei20@gmail.com>
2022-02-07 20:02:11 +0000

    index update

--- index.js
+++ index.js
@@ -2,78 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
 const req = require("express/lib/request");
+const {
+  listBankController,
+  creatBankController,
+  updateBankController,
+  deleteBankController,
+} = require("./controllers");
 
-const server = express();
+
+//create express
+  const server = express();
 
 //Middleware
 server.use(bodyParser.json());
-
-//DataBase
-
-let  banksDb = [];
-
-//Bankmodel
-
-class Bankmodel {
-  constructor({ name, location, branch, phone, address, accountNumber }) {
-    this.name = name;
-    this.location = location;
-    this.branch = branch;
-    this.phone = phone;
-    this.address = address;
-    this.accountNumber = accountNumber;
-  }
-  save() {
-    banksDb.push(this);
-    return this;
-  }
-
-  static all() {
-    return banksDb;
-  }
-  static update(updateInfo = {}) {
-    banksDb.map((bank) => {
-      if (bank.name === updateInfo.name) {
-        return { ...bank, ...updateInfo };
-      }
-      return bank;
-    });
-  }
-}
-
-//Controllers
-const listBankController = (req, res) => {
-  const banks = Bankmodel.all();
-  res.json({ data: banks });
-};
-const creatBankController = (req, res) => {
-  const { name, location, branch, phone, address, accountNumber } = req.body;
-  const bank = new Bankmodel({
-    name,
-    location,
-    branch,
-    phone,
-    address,
-    accountNumber,
-  });
-  bank.save();
-  res.json({ message: "create Successful", data: bank });
-};
-const updateBankController = (req, res) => {
-  const { name, location, branch, phone, address, accountNumber } = req.body;
-
-  const updatedBank = Bankmodel.update({
-    name,
-    location,
-    branch,
-    phone,
-    address,
-    accountNumber,
-  });
-
-  res.json({ message: "updated successful", data: updatedBank });
-};
-const deleteBankController = (req, res) => {};
 
 //Routes
 //view - get method
