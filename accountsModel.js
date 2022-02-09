197cca3e61c4fbb1ccead9e5707278daec873155
kencal20 <kenadjei20@gmail.com>
2022-02-09 18:13:55 +0000

    add accountsModel

---
+++ accountsModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AccountSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Acoount", AccountSchema);
