1c9ef04f7eaf3d6a8666c2d151234306a1fef1a7
kencal20 <kenadjei20@gmail.com>
2022-02-11 12:24:58 +0000

    bank controller creation and organization

---
+++ Controllers/bank.js
@@ -0,0 +1,81 @@
+const Bankmodel = require("../Models/bank");
+const Account = require("../Models/account");
+const listBankController = (req, res) => {
+  //list banks
+  const { id } = req.params;
+
+  if (id) {
+    Bankmodel.find({ _id: id })
+      .then((banks) => {
+        res.json({ data: banks });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    Bankmodel.find()
+      .then((banks) => {
+        res.json({ data: banks });
+      })
+      .catch((err) => console.log(err));
+  }
+};
+const creatBankController = (req, res) => {
+  const { name, location, branch, phone, address, accountNumber } = req.body;
+  const bank = new Bankmodel({
+    name,
+    location,
+    branch,
+    phone,
+    address,
+    accountNumber,
+  });
+  bank
+    .save()
+    .then((result) => {
+      res.json({ message: "create Successful", data: result });
+    })
+    .catch((error) => console.log(error));
+};
+const updateBankController = (req, res) => {
+  const { id, name, location, branch, phone, address, accountNumber } =
+    req.body;
+
+  Bankmodel.findById(id)
+    .then((bank) => {
+      if (bank) {
+        bank.name = name;
+        bank.location = location;
+        bank.branch = branch;
+        bank.phone = phone;
+        bank.address = address;
+        bank.accountNumber = accountNumber;
+
+        bank.save();
+        res.json({ message: "updated successful", data: bank });
+      }
+      res.json({ message: "Document cannot be found" });
+    })
+    .catch((err) => console.log(err));
+};
+
+const deleteBankController = (req, res) => {
+  const { id } = req.body;
+  Bankmodel.findByIdAndRemove(id).then((deletedBank) => {
+    if (deletedBank) {
+      AccountsModel.deleteMany({ bankId: deletedBank._id })
+        .then((result) => {
+          res.json({ message: "bank deleted", data: deletedBank });
+        })
+        .catch((err) => console.log(err));
+
+      return;
+    }
+    res.json({ message: "bank not found" });
+  });
+};
+
+module.exports = {
+  listBankController,
+  creatBankController,
+  updateBankController,
+  deleteBankController,
+};
