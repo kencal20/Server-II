4c5a1184a84e4985ccb6a594fd4e25a5da798f67
kencal20 <kenadjei20@gmail.com>
2022-02-12 20:33:14 +0000

    express validator in route

--- Routes/bank.js
+++ Routes/bank.js
@@ -5,12 +5,13 @@ const {
   updateBankController,
   deleteBankController,
 } = require("../Controllers/bank");
+const {body} = require("express-validator");
 const router = express.Router();
 
 // view - get method
 router.get("/bank/:id?", listBankController);
 //create - post method
-router.post("/bank", creatBankController);
+router.post("/bank",body('name').trim().not().isEmpty(), creatBankController);
 //update - put method
 router.put("/bank", updateBankController);
 // //delete - delete method
